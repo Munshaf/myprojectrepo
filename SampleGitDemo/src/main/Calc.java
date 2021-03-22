fb7b702ca80f488ab5651321718b2498ac4a0038
a <a@DESKTOP-QOPC76O>
2021-03-22 05:39:35 -0700

    added add function

--- SampleGitDemo/src/main/Calc.java
+++ SampleGitDemo/src/main/Calc.java
@@ -4,5 +4,9 @@ public class Calc {
 	public static void main(String arg[]) {
 		System.out.println("hello!!");
 	}
+	
+	public static void add(int num1,int num2){
+	      System.out.println(num1+num2);
+	  }
 
 }
