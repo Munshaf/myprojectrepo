af66826b2fd9560411962f4d832a754d70af4e9a
a <a@DESKTOP-QOPC76O>
2021-03-22 05:57:28 -0700

    added some

--- SampleGitDemo/src/main/Calc.java
+++ SampleGitDemo/src/main/Calc.java
@@ -3,6 +3,7 @@ package main;
 public class Calc {
 	public static void main(String arg[]) {
 		System.out.println("hello!!");
+		System.out.println("added some code!!");
 	}
 	
 	public static void add(int num1,int num2){
