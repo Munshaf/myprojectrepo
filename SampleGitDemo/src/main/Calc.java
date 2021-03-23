57d352cf5c14480faf0ba83fb346e404849182e5
a <a@DESKTOP-QOPC76O>
2021-03-22 21:08:24 -0700

    added prod function to br1

--- SampleGitDemo/src/main/Calc.java
+++ SampleGitDemo/src/main/Calc.java
@@ -12,6 +12,9 @@ public class Calc {
 	public static void diff(int num1,int num2){
 	      System.out.println(num1-num2);
 	  }
+	public static void prod(int num1,int num2){
+	      System.out.println(num1*num2);
+	  }
 
 
 }
